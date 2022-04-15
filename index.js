4e11169b330e7e2108ac979d8f80e3a3b23bc7a1
LincolnMcloud <lamcloudjr@gmail.com>
2022-04-15 15:03:59 -0400

    7th commit

--- index.js
+++ index.js
@@ -1,11 +1,33 @@
 const express = require('express')
+const http = require('http');
 const app = express()
+const server = http.createServer(app);
+const { Server } = require("socket.io");
+const io = new Server(server);
+
+var dummyData = require("./dummyData.json")
+
+
 const port = 3000
 
 app.get('/', (req, res) => {
   res.sendFile(__dirname + '/templates/index.html')
 })
 
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
+app.get('/selectCelebs', (req, res) => {
+  res.sendFile(__dirname + '/templates/selectCelebs.html')
 })
+
+app.get("/celebData", (req, res) =>{
+  keys = Object.keys(dummyData)
+
+  res.json(Object.keys(dummyData))
+})
+
+io.on('connection', (socket) => {
+  console.log('a user connected');
+});
+
+server.listen(port, () => {
+  console.log('listening on *:3000');
+});
